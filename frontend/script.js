48f84517a542121420fd79d4d2d910d662fb0875
Raju Pitta <appalarajupitta9@gmail.com>
2026-07-06 21:58:59 +0530

    updatr fronyend for prodution

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://smart-lender-g9fm.onrender.com/";
 
 const form = document.getElementById("loanForm");
 const result = document.getElementById("result");
